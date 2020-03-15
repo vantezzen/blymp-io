10a99f92cbd44d08d51ef940e3a92890dcc10314
Bennett <hello@vantezzen.io>
2020-03-15 15:46:20 +0100

    Enable compression

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const helmet = require('helmet');
+const compression = require('compression');
 const http = require('http');
 const socketIo = require('socket.io');
 const setupSocket = require('./socket');
 
 const app = express();
 app.use(helmet());
+app.use(compression());
 const server = http.createServer(app);
 
 // Setup socket server
